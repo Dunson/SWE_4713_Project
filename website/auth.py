8f777d50f6a4cd68d4dbd76d28313127b9a11e5f
Brandon Dunson <brandondunson15@gmail.com>
2021-10-16 00:21:43 -0400

    Chart of accounts UI added. Can add acounts to usr

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,8 @@
 from os import error
+#from typing_extensions import Required
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from sqlalchemy.orm import query
-from .models import User
+from .models import User, Account
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
@@ -13,7 +14,7 @@ auth = Blueprint('auth', __name__)
 searchID = 'none'
 
 
-#Back-end for logging in
+# Back-end for logging in
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -26,10 +27,11 @@ def login():
         if user:
 
             if user.status == False:
-                flash('Your account must be activated by an administrator.', category='error')
+                flash('Your account must be activated by an administrator.',
+                      category='error')
                 return redirect(url_for('auth.login'))
-            
-            if user.hasAdmin == True:
+
+            if user.hasAdmin == True and check_password_hash(user.password, password):
                 flash('Admin login successful!', category='success')
                 login_user(user)
                 return redirect(url_for('auth.adminPort'))
@@ -37,14 +39,14 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Login Succeful!', category='success')
                 login_user(user)
-       
+
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect Password!', category='error')
 
         else:
             flash('Required fields are empty!', category='error')
-    return render_template("login.html", user = current_user)
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
@@ -52,7 +54,6 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
-
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -64,58 +65,42 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        
-        #Generates userName based on firstname, lastname and month&year of when account was created
-        def userNameGen(first, last):
-            first = firstName
-            last = lastName
-            currMonth = str(datetime.now().month)
-            currYear = str(datetime.now().year)
-
-            if len(currMonth) < 2:
-                currMonth = '0' + currMonth
-            
-            userName = first[0] + last + currMonth + currYear[2] + currYear[3]
-            return userName
-        
-
         user = User.query.filter_by(email=email).first()
         pwd_check = User.password_check(password1)
 
-
-        # The password conditions still need to satisfy the requirements
+        # Creation validation logic
         if user:
             flash('An account with that email already exists!', category='error')
         elif len(firstName) < 2:
             flash('First name must be greater than 1 character', category='error')
-        elif password1 != password2: # This compares the two passwords
+        elif password1 != password2:  # This compares the two passwords
             flash('Passwords do not match', category='error')
-        elif not pwd_check:  
+        elif not pwd_check:
             flash('Password does not meet the requirements', category='error')
         else:
             # Add user to database
-            new_user = User(email=email, firstName=firstName, lastName=lastName, 
-                password=generate_password_hash(password1, method='sha256'), 
-                userName = userNameGen(firstName, lastName),
-                hasAdmin = False, hasMan = False, status = False, 
-                creationDate = datetime.now())
-            
+            new_user = User(email=email, firstName=firstName, lastName=lastName,
+                            password=generate_password_hash(
+                                password1, method='sha256'),
+                            userName=userNameGenGlobal(firstName, lastName),
+                            hasAdmin=False, hasMan=False, status=False,
+                            creationDate=datetime.now())
+
             db.session.add(new_user)
-            db.session.commit()     
-            
-            flash('Account Created', category='success')
+            db.session.commit()
+
+            flash('Account Created! Note: The admininstrator must activate your account before you can login.', category='success')
             return redirect(url_for('auth.login'))
 
-    
-    return render_template("signUp.html", user = current_user)
+    return render_template("signUp.html", user=current_user)
 
 
-@auth.route('/recovery', methods = ['GET', 'POST'])
+@auth.route('/recovery', methods=['GET', 'POST'])
 def recovery_Page():
     if request.method == 'POST':
         email = request.form.get('email')
-        
-        #Check if email is in database
+
+        # Check if email is in database
         user = User.query.filter_by(email=email).first()
         if not user:
             flash('That email was not found in our records.', category='error')
@@ -124,17 +109,16 @@ def recovery_Page():
             flash('Recovery email sent!', category='success')
             return redirect(url_for('auth.login'))
 
-    return render_template('passwordRecov.html', user= current_user)
+    return render_template('passwordRecov.html', user=current_user)
 
 
-
-@auth.route('/reset_verified/<token>', methods = ['GET', 'POST'])
+@auth.route('/reset_verified/<token>', methods=['GET', 'POST'])
 def reset_password(token):
 
     user = User.verify_reset_token(token)
 
     if not user:
-        flash('Reset Token Expired!',category='error')
+        flash('Reset Token Expired!', category='error')
         return redirect(url_for('auth.login'))
 
     password1 = request.form.get('password1')
@@ -146,7 +130,7 @@ def reset_password(token):
 
             if check_password_hash(user.oldPassword, password1):
                 flash('You can not reuse an old password!', category='error')
-                return redirect(url_for('auth.reset_password', token = token))
+                return redirect(url_for('auth.reset_password', token=token))
 
             user.reset_password(password1, commit=True)
             flash('Password Reset Successful!!', category='success')
@@ -154,18 +138,16 @@ def reset_password(token):
         else:
             flash('Passwords must match!', category='error')
 
-    return render_template('reset_verified.html', user=current_user)   
+    return render_template('reset_verified.html', user=current_user)
 
 
-
-
-#Method for admin dashboard
-@auth.route('/adminPortal', methods = ['GET', 'POST'])
+# Method for admin dashboard
+@auth.route('/adminPortal', methods=['GET', 'POST'])
 @login_required
 def adminPort():
 
     #qID = 0
-    #Logic for updating accounts
+    # Logic for updating accounts
     if request.method == 'POST':
         global searchID
         searchID = request.form.get('searchBar')
@@ -173,70 +155,114 @@ def adminPort():
             flash('Search field can not be blank.', category='error')
             return redirect(url_for('auth.adminPort'))
         else:
-            return redirect(url_for('auth.accountEdit'))
+            return redirect(url_for('auth.accountOverview'))
 
-    
     else:
 
         if current_user.hasAdmin:
-            return render_template('adminPortal.html', 
-                user = current_user, query = User.query.all())
+            return render_template('adminPortal.html',
+                                   user=current_user, query=User.query.all())
         else:
             flash('You do not have access to this page.', category='error')
             return redirect(url_for('views.home'))
 
 
-@auth.route('/editAccount', methods = ['GET', 'POST'])
+
+
+@auth.route('/accountOverview', methods=['GET', 'POST'])
 @login_required
-def accountEdit():
+def accountOverview():
 
-    usr_status = False
-    usr_hasMan = False
-    usr_hasAdmin = False
+    if current_user.hasAdmin:
 
-    qID = int(searchID)
-    user_to_update = User.query.filter_by(id = qID).first()
+        usr_status = False
+        usr_hasMan = False
+        usr_hasAdmin = False
 
-    if request.method == 'POST':
+        qID = int(searchID)
+        user_to_update = User.query.filter_by(id=qID).first()
 
-        email = request.form.get("email")
-        firstName = request.form.get("firstName")
-        lastName = request.form.get("lastName")
-        status = request.form.get("activeButton")
-        hasMan = request.form.get("manButton")
-        hasAdmin = request.form.get("adminButton")
+        if request.method == 'POST':
 
-        if status == 'on':
-            usr_status = True
-        if hasMan == 'on':
-            usr_hasMan = True
-        if hasAdmin == 'on':
-            usr_hasAdmin = True
+            email = request.form.get("email")
+            firstName = request.form.get("firstName")
+            lastName = request.form.get("lastName")
+            status = request.form.get("activeButton")
+            hasMan = request.form.get("manButton")
+            hasAdmin = request.form.get("adminButton")
 
-        userName = userNameGenGlobal(firstName, lastName)
-        if len(firstName) < 2 or len(lastName) < 2 or len(email) < 1:
-            flash('Input fields can not be blank.', category='error')
-        else:
-            
-            updateStat = user_to_update.update_user(userName, email, firstName, lastName, usr_hasMan, usr_hasAdmin, usr_status, commit=True)
+            if status == 'on':
+                usr_status = True
+            if hasMan == 'on':
+                usr_hasMan = True
+            if hasAdmin == 'on':
+                usr_hasAdmin = True
+
+            if len(firstName) < 2 or len(lastName) < 2 or len(email) < 1:
+                flash('Input fields can not be blank.', category='error')
+                return redirect(url_for('auth.accountOverview'))
+            else:
+                userName = userNameGenGlobal(firstName, lastName)
+
+                updateStat = user_to_update.update_user(
+                                userName, email, firstName, 
+                                lastName, usr_hasMan, usr_hasAdmin, 
+                                usr_status, commit=True)
             if updateStat:
                 flash('Account updated successfully', category='success')
-                return redirect(url_for('auth.adminPort'))
+                return redirect(url_for('auth.accountOverview'))
             else:
                 flash('Account Update Failed!', category='error')
-        
+    else:
+        flash('You do not have access to this page.', category='error')
+        return redirect(url_for('views.home'))    
+
+    return render_template('accountOverview.html', user=current_user, 
+                        query=User.query.all(), searchID=searchID, 
+                        acc_query = Account.query.join(User).filter(Account.user_id==searchID))
+
+
+#FORMAT monetary values to only use 2 decimal places by: 
+#formated_float = "{:.2f}.format(float_variable)"
+@auth.route('/newAccount', methods = ['GET', 'POST'])
+@login_required
+def newChart():
+
+
+    qID = int(searchID)
     
-    return render_template('editAccount.html', user=current_user, query = User.query.all(), searchID = searchID)
+
+    if request.method == 'POST':
+        acc_name = request.form.get('acc_name')
+        acc_cat = request.form.get('acc_cat')
+        acc_desc = request.form.get('acc_desc')
+        init_bal = request.form.get('init_bal')
+        acc_statement = request.form.get('acc_statement')
+        
+        new_acc = Account(acc_name = acc_name, acc_cat = acc_cat, 
+                            acc_desc = acc_desc, init_bal = init_bal,
+                            acc_statement = acc_statement, 
+                            user_id = qID)
+        
+        db.session.add(new_acc)
+        db.session.commit()
+        return redirect(url_for('auth.accountOverview'))
+    
+
+    return render_template('newAccount.html', user = current_user, 
+                        query = User.query.all(), 
+                        searchID = searchID)
 
 
+
+
+#Username generator
 def userNameGenGlobal(first, last):
-            first = first
-            last = last
-            currMonth = str(datetime.now().month)
-            currYear = str(datetime.now().year)
+    currMonth = str(datetime.now().month)
+    currYear = str(datetime.now().year)
 
-            if len(currMonth) < 2:
-                currMonth = '0' + currMonth
-            
-            userName = first[0] + last + currMonth + currYear[2] + currYear[3]
-            return userName
+    if len(currMonth) < 2:
+        currMonth = '0' + currMonth
+
+    userName = first[0] + last + currMonth + currYear[2] + currYear[3]
+    return userName
--- website/models.py
+++ website/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import backref
 from werkzeug.datastructures import _CacheControl
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db 
@@ -10,8 +11,7 @@ import re
 from datetime import datetime, timedelta
 
 
-
-#Defined User Model for database
+#Defined User table for database
 class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True) #unique identifier
@@ -25,6 +25,7 @@ class User(db.Model, UserMixin):
     hasMan = db.Column(db.Boolean, default = False)
     status = db.Column(db.Boolean, default = False)
     creationDate = db.Column(db.Date())
+    accounts = db.relationship("Account", backref="parent")
 
     
 
@@ -53,7 +54,7 @@ class User(db.Model, UserMixin):
 
     #Method for password validation
     #This method was supplied by: 
-    def password_check(password):
+    def password_check(password, passwd2):
         
         length_error = len(password) < 8
 
@@ -69,6 +70,7 @@ class User(db.Model, UserMixin):
 
         return password_ok
 
+
     def update_user(self, usrName, usrEmail, usrFirst, usrLast, usrMan, usrAdmin, usrStat, commit=False):
     
         self.firstName = usrFirst
@@ -83,18 +85,36 @@ class User(db.Model, UserMixin):
             db.session.commit()
             return True
 
+    """
+    def password_expire():
+        # put DB column here. SELECT password_exp FROM tablename WHERE id = userID
+        password_origin = datetime.now() + timedelta(days=365)
+        password_exp = datetime.now()
+        password_notification = password_origin - timedelta(days=3)
+    """
 
+class Account(db.Model):
+    
+    acc_num = db.Column(db.Integer, primary_key=True) #unique identifier. Needs adjusting
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
 
+    acc_name = db.Column(db.String(150), unique = True)
+    acc_desc = db.Column(db.String(150))
+    acc_cat = db.Column(db.String(150))
+    acc_sub_cat = db.Column(db.String(150))
+   
+    init_bal = db.Column(db.Float)
+    acc_bal = db.Column(db.Float)
+    acc_deb = db.Column(db.Float)
+    acc_cred = db.Column(db.Float)
 
+    acc_statement = db.Column(db.String(150))
+    acc_order = db.Column(db.Integer)
 
+    creation_date = db.Column(db.Date())
+    creation_time = db.Column(db.Time(), nullable = False)
 
+    acc_status = db.Column(db.Boolean, default = False)
+    acc_comment = db.Column(db.String(150))
 
-
-
-"""
-def password_expire():
-    # put DB column here. SELECT password_exp FROM tablename WHERE id = userID\
-    password_origin = datetime.now() + timedelta(days=365)
-    password_exp = datetime.now()
-    password_notification = password_origin - timedelta(days=3)
-"""
+    
